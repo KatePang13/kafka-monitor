88e374a3ffbc19a2daac8e6845d5f2dd68d1d0e6
ketepang13 <liuyhpangpang@gmail.com>
2020-12-22 16:12:11 +0800

    自动转换工具  dynatrace  plugin.json --> jmx_exporter plugin.yaml

---
+++ tools/transform.py
@@ -0,0 +1,29 @@
+import simplejson as json
+import yaml
+
+ymlData = { 'lowercaseOutputName': True, 'rules' : []}
+    
+fjson = open('plugin.json', 'r')
+jsonData = json.load(fjson)
+fjson.close()
+
+metrics = jsonData["metrics"]
+for metric in metrics :
+    key = metric["timeseries"]["key"]
+    domain = metric["source"]["domain"]
+    attribute = metric["source"]["attribute"]
+    keyProperties = metric["source"]["keyProperties"]
+    keyProperties = '{keyProperties}'.format(keyProperties=keyProperties)
+    keyProperties = keyProperties.replace('\'', '')
+    keyProperties = keyProperties.replace('{', '')
+    keyProperties = keyProperties.replace('}', '')
+    keyProperties = keyProperties.replace(': ', '=')
+    pattern = '{domain}<{keyProperties}><>{attribute}'.format(domain=domain, keyProperties=keyProperties, attribute=attribute)
+    #print(key, pattern)
+    rule = { 'pattern' : pattern , 'name':'pang.'+key,  'type': 'GAUGE'}
+    ymlData['rules'].append(rule)
+
+print(ymlData)
+with open(r'plugin.yaml', 'w') as file:
+    documents = yaml.dump(ymlData, file)
+    file.close()
